ac9555490c8634dced84a6c1ff7505474affce8b
yaoguopeng <l>
2019-08-22 17:07:25 +0800

    开始从后台获取数据

--- interest/app.js
+++ interest/app.js
@@ -11,6 +11,38 @@ App({
         traceUser: true,
       })
     }
+
+    // 展示本地存储能力
+    var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              // 可以将 res 发送给后台解码出 unionId
+              this.globalData.userInfo = res.userInfo
+
+              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+              // 所以此处加入 callback 以防止这种情况
+              if (this.userInfoReadyCallback) {
+                this.userInfoReadyCallback(res)
+              }
+            }
+          })
+        }
+      }
+    })
   },
   onShow(opts) {
     console.log('App Show', opts)
@@ -54,18 +86,9 @@ App({
       })
     }
   },
-  // 通过云函数获取用户 openid，支持回调或 Promise
-  getUserOpenIdViaCloud() {
-    return wx.cloud.callFunction({
-      name: 'wxContext',
-      data: {}
-    }).then(res => {
-      this.globalData.openid = res.result.openid
-      return res.result.openid
-    })
-  },
   globalData: {
     userInfo: null,
+    islandIndex: {},
     web_url: 'http://192.168.1.107:2013/'
   }
 })
---
+++ interest/page/start/start.js
@@ -0,0 +1,63 @@
+//login.js
+//获取应用实例
+const app = getApp()
+const web_url = getApp().globalData.web_url;
+
+Page({
+  data: {
+    remind: '加载中',
+    angle: 0,
+    userInfo: {},
+    islandIndex:''
+  },
+  goToIndex: function() {
+    wx.switchTab({
+      url: '/page/home/index',
+    });
+  },
+  onLoad: function() {
+    var that = this
+    wx.setNavigationBarTitle({
+      title: wx.getStorageSync('mallName')
+    })
+    console.log('onLoad  ', this.islandIndex);
+  },
+  onShow: function() {
+    // 获取islandIndex
+    let _self = this;
+    wx.request({
+      url: web_url + '/island/index',
+      data: {
+      },
+      header: { 'content-type': 'application/json' },
+      method: 'GET',
+      dataType: 'json',
+      success: function (res) {
+        _self.setData({
+          islandIndex : res.data.entity
+        });
+      },
+    })
+  },
+  onReady: function() {
+    var that = this;
+    setTimeout(function() {
+      that.setData({
+        remind: ''
+      });
+    }, 1000);
+    wx.onAccelerometerChange(function(res) {
+      var angle = -(res.x * 30).toFixed(1);
+      if (angle > 14) {
+        angle = 14;
+      } else if (angle < -14) {
+        angle = -14;
+      }
+      if (that.data.angle !== angle) {
+        that.setData({
+          angle: angle
+        });
+      }
+    });
+  }
+});
