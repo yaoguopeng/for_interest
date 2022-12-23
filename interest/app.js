2ed642ae8ba2d0ecd1e9b0ca86a9e342ceba4c53
yaoguopeng <helloyaogp@163.com>
2022-12-23 16:21:17 +0800

    修改

--- interest/app.js
+++ interest/app.js
@@ -2,6 +2,24 @@
 const config = require('./config')
 
 App({
+  async onLaunch() {
+    // 使用 callContainer 前一定要 init 一下，全局执行一次即可
+    wx.cloud.init()
+    // 下面的请求可以在页面任意一处使用
+    const result = await wx.cloud.callContainer({
+      config: {
+        env: 'prod-4gh1tzgl11be006b', // 微信云托管的环境ID
+      },
+      path: '/', // 填入业务自定义路径和参数，根目录，就是 / 
+      method: 'GET', // 按照自己的业务开发，选择对应的方法
+      header: {
+        'X-WX-SERVICE': 'springboot-xdel', // xxx中填入服务名称（微信云托管 - 服务管理 - 服务列表 - 服务名称）
+      }
+      // dataType:'text', // 默认不填是以 JSON 形式解析返回结果，若不想让 SDK 自己解析，可以填text
+    })
+    console.log(result)
+  },
+
   onLaunch(opts) {
     console.log('App Launch', opts)
     if (!wx.cloud) {
@@ -90,6 +108,7 @@ App({
     islandMusic: {},
     bgm_url: '',// 背景音乐路径
     bgm_status: false, // 背景音乐播放状态
-    web_url: 'http://192.168.1.107:2013/'
+    web_url: 'http://127.0.0.1:2013/'
+    //  web_url: 'https://springboot-xdel-24085-6-1300055838.sh.run.tcloudbase.com:2013/'
   }
 })
